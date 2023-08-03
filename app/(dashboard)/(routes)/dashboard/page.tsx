83cc4186f82cc8c29974b111245ae2565ce85969
Andrzej Darowski <andrzej.dar@gmail.com>
2023-08-03 20:19:03 +0200

    open ai setup

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,64 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { MessageSquare, ArrowRight, ImageIcon, Music } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "con",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "con2",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/con2",
+  },
+  {
+    label: "con3",
+    icon: ImageIcon,
+    color: "text-red-500",
+    bgColor: "bg-red-500/10",
+    href: "/con3",
+  },
+];
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <>
-      <p className="text-6xl text-green-500"> Dashboard</p>
-      <Button variant="destructive">test</Button>
-    </>
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold">asdfasdfas</h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          asdfasdf asdfasdfas dsf a sdf sdfdasfdsfas adsf
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => {
+              router.push(tool.href);
+            }}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
+    </div>
   );
 }
