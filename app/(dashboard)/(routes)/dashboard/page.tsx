95eba50a3558c2e03f11011278c12008d88bbc12
Andrzej Darowski <andrzej.dar@gmail.com>
2023-08-03 13:18:13 +0200

    components setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <p className="text-6xl text-green-500"> Dashboard</p>
+      <Button variant="destructive">test</Button>
+    </>
+  );
+}
